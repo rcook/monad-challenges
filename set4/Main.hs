46a067de3e960d36006d338900d725984cc10508
Richard Cook <rcook@rcook.org>
2018-01-27 14:09:04 -0800

    Set 4: Common abstraction

---
+++ set4/Main.hs
@@ -0,0 +1,11 @@
+{-# LANGUAGE MonadComprehensions #-}
+{-# LANGUAGE NoImplicitPrelude #-}
+--{-# LANGUAGE RebindableSyntax  #-}
+
+module Main (main) where
+
+import MCPrelude
+import System.IO (IO, print)
+
+main :: IO ()
+main = print "HELLO"
